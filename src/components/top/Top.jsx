be6fe0cd31e6c427080c43ac51ba2f8c33a636d4
Alan Ribeca <alanribeca2003@gmail.com>
2024-10-16 21:09:48 -0300

    empezando con el main

--- src/components/top/Top.jsx
+++ src/components/top/Top.jsx
@@ -1,6 +1,19 @@
 import "./top.scss";
 export const Top = () => {
-  return <section className="top">
-    
-  </section>;
+  return (
+    <section className="top">
+      <div className="containerImg">
+        <img
+          src="../public/img/princi3.png"
+          alt="imagen de una plata"
+          className="imgPrinci"
+        />
+      </div>
+      <div className="containerTitle">
+        <h1 className="titleImpresa">Nursery</h1>
+        <h2 className="titleCursiva">Nursery</h2>
+        <h2 className="titlePrincipal">Guia de cuidado</h2>
+      </div>
+    </section>
+  );
 };
